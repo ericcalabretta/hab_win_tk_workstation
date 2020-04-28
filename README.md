b06735b1e754e4a11d454dc7b60f1fa497bd89c5
ericcalabretta <eric.calabretta@gmail.com>
2020-04-28 14:47:49 -0500

    added more stuff

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # hab_win_tk_workstation
 
 TODO: Enter the cookbook description here.
-
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,43 @@
 include_recipe 'chocolatey::default'
 
-chocolatey_package 'GoogleChrome' do
-  options '--ignorechecksum'
-end
-chocolatey_package 'VisualStudioCode'
+#chrome fails wich chocolatey resource 
+# chocolatey_package 'googlechrome' do
+#   options '--ignorechecksum'
+# end
+
+chocolatey_package 'vscode'
 
 chocolatey_package 'git'
 
-chocolatey_package 'Habitat'
+hab_install 'install habitat' do
+  license 'accept'
+end
+
+directory 'C:\hab\cache\keys' do
+  recursive true
+  action :create
+end
+
+# Set value as your origin
+# windows_env 'HAB_ORIGIN' do
+#   value ""
+# end
+
+# Set vaule as your auth token
+# windows_env 'HAB_AUTH_TOKEN' do
+#   value ""
+# end
+
+# Set your origin not eric
+# execute 'download key' do
+#   command 'hab origin key download eric'
+# end
+
+#set your origin not eric
+# execute 'download secret key' do
+#   command 'hab origin key download eric -s'
+# end
+
+execute 'download studio image' do
+  command 'docker pull habitat/win-studio-x86_64-windows'
+end
